20d40c6447185ecacf651df2fd69a180676fe2a7
Jack Hargreaves <jahar@netcompany.com>
2022-04-11 13:15:34 +0100

    Use springboot ConnectionFactory for configuring RMQ
    This actually works, rather than settings being ignored.

--- src/main/java/com/foo/library/rmq/ConnectionConfig.java
+++ src/main/java/com/foo/library/rmq/ConnectionConfig.java
@@ -1,24 +1,20 @@
 package com.foo.library.rmq;
 
-import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
+
+
 @Configuration
 public class ConnectionConfig {
 	@Bean
-	public static ConnectionFactory createRabbitConnectionFactory(
-		@Value("${rmq.host:localhost}") String host,
-		@Value("${rmq.port:5672}") int port,
-		@Value("${rmq.username:guest}") String username,
-		@Value("${rmq.password:guest}") String password
-	) {
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost(host);
-		factory.setPort(port);
-		factory.setUsername(username);
-		factory.setPassword(password);
-		return factory;
+	public static ConnectionFactory rabbitConnectionFactory(
+		@Value("${rmq.dsn:amqp://}") String dsn
+	) throws Exception {
+		return new CachingConnectionFactory(new URI(dsn));
 	}
 }
